78249190459b70b137222039cc2b1438e75b6954
Roberto <rcastellong98@gmail.com>
2023-04-04 18:37:17 +0200

    [add] FeaturedRooms

--- src/components/FeaturedRooms.jsx
+++ src/components/FeaturedRooms.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { useRoomsData } from '../context/RoomContext';
+import Loading from './Loading';
+import Room from './Room';
+import Title from './Title';
 
 const FeaturedRooms = () => {
-  const { featuredRooms, rooms } = useRoomsData();
+  const { featuredRooms, loading } = useRoomsData();
 
-  return <div></div>;
+  return (
+    <section className='featured-rooms'>
+      <Title title={'Featured Rooms'} />
+      <div className='featured-rooms-center'>
+        {loading ? <Loading /> : featuredRooms.map((room) => <Room key={room.id} room={room} />)}
+      </div>
+    </section>
+  );
 };
 
 export default FeaturedRooms;
